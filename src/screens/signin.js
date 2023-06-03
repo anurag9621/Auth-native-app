856683bd4b0c582350a399c21422200ec4b67a53
Anurag Pandey <arushpandey123buy@gmail.com>
2023-06-03 17:47:00 +0530

    add screen of signin page

--- src/screens/signin.js
+++ src/screens/signin.js
@@ -3,8 +3,8 @@ import {
   Text,
   Image,
   StyleSheet,
-    useWindowDimensions,
-  ScrollView
+  useWindowDimensions,
+  ScrollView,
 } from "react-native";
 import React, { useState } from "react";
 import Logo from "../../assets/logo.png";
@@ -29,61 +29,61 @@ const Signin = () => {
   const onSignInWithApplePressed = () => {
     console.warn("sing in with apple");
   };
-    const onSignUpPressed = () => {
-      console.warn("onSignUpPressed");
-    };
-    return (
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={styles.root}>
-          <Image
-            source={Logo}
-            style={[styles.logo, { height: height * 0.4 }]}
-            resizeMode="contain"
-          />
-          <CustomInput
-            placeholder="Username"
-            value={username}
-            setvalue={setUsername}
-            secureTextEntry={false}
-          />
-          <CustomInput
-            placeholder="Password"
-            value={password}
-            setvalue={setPassword}
-            secureTextEntry={true}
-          />
-          <CustomButton text="Sign In" onPress={onSignInPressed} />
-          <CustomButton
-            text="Forgot password?"
-            onPress={onForgotPasswordPressed}
-            type="TERTIARY"
-          />
-          <CustomButton
-            text="Sign In with Facebook"
-            onPress={onSignInWithFacebookPressed}
-            bgColor="#E7EAF4"
-            fgColor="#4765A9"
-          />
-          <CustomButton
-            text="Sign In with google"
-            onPress={onSignInWithGooglePressed}
-            bgColor="#FAE9EA"
-            fgColor="#DD4D44"
-          />
-          <CustomButton
-            text="Sign In with Apple"
-            onPress={onSignInWithApplePressed}
-            bgColor="#e3e3e3"
-            fgColor="#363636"
-          />
-          <CustomButton
-            text="Don't have an account? Create one"
-            onPress={onSignUpPressed}
-            type="TERTIARY"
-          />
-        </View>
-      </ScrollView>
-    );
+  const onSignUpPressed = () => {
+    console.warn("onSignUpPressed");
+  };
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.root}>
+        <Image
+          source={Logo}
+          style={[styles.logo, { height: height * 0.4 }]}
+          resizeMode="contain"
+        />
+        <CustomInput
+          placeholder="Username"
+          value={username}
+          setvalue={setUsername}
+          secureTextEntry={false}
+        />
+        <CustomInput
+          placeholder="Password"
+          value={password}
+          setvalue={setPassword}
+          secureTextEntry={true}
+        />
+        <CustomButton text="Sign In" onPress={onSignInPressed} />
+        <CustomButton
+          text="Forgot password?"
+          onPress={onForgotPasswordPressed}
+          type="TERTIARY"
+        />
+        <CustomButton
+          text="Sign In with Facebook"
+          onPress={onSignInWithFacebookPressed}
+          bgColor="#E7EAF4"
+          fgColor="#4765A9"
+        />
+        <CustomButton
+          text="Sign In with google"
+          onPress={onSignInWithGooglePressed}
+          bgColor="#FAE9EA"
+          fgColor="#DD4D44"
+        />
+        <CustomButton
+          text="Sign In with Apple"
+          onPress={onSignInWithApplePressed}
+          bgColor="#e3e3e3"
+          fgColor="#363636"
+        />
+        <CustomButton
+          text="Don't have an account? Create one"
+          onPress={onSignUpPressed}
+          type="TERTIARY"
+        />
+      </View>
+    </ScrollView>
+  );
 };
 const styles = StyleSheet.create({
   root: {
